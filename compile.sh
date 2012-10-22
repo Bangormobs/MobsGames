c31cff6450a7f7e0a9774a5a074f5ec53582701e
triggerhapp <triggerhapp@hgs.uk.to>
2012-10-23 00:05:52 +0100

    Added an automated compile and distribute script. Users of this will need to alter the location of their own Bukkit jar file and the copy of IRCChat. Compiles to MobsGames-0.1-(Commit Version Number).jar

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+revision=`git log --pretty=format:'' | wc -l`
+classpath='-cp ../IRCChat/bin/com/trigg/irc/:/home/triggerhapp/Downloads/bukkit-1.2.5-R4.1-20120708.060130-22.jar'
+
+find src -name \*.java -print > file.list
+javac ${classpath} @file.list
+file="MobsGames-0.1-${revision}.jar"
+if [ -f $file ];
+then
+  echo 'A Jar file already exists for this version. Does the git need a commit?'
+else
+  cd src
+  jar cvf ../${file} * ../*.yml
+  scp ../${file} tekkit@mobsoc.co.uk:/home/tekkit/www/plugins
+fi
+
